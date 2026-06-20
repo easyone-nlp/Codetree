c33321c2b4b58d90caad93ffd5002f0879672f94
leejiwon <leejiwon2325@gmail.com>
2026-06-20 14:44:32 +0900

    [Passed] 100점 도달하기 (52ms, 16MB)

---
+++ trail0/100점 도달하기/reach-100-point.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+for i in range(N, 101):
+    if i < 60:
+        print('F', end=' ')
+    elif i < 70:
+        print('D', end=' ')
+    elif i < 80:
+        print('C', end=' ')
+    elif i < 90:
+        print('B', end=' ')
+    else:
+        print('A', end=' ')
